daf4c97433fcd0fa224cc616e2cf70d6d919e2c9
Nicolas Penagos <nikopm98@hotmail.com>
2019-08-25 01:30:00 -0500

    The method that creates and multiplies n matrices is to implement

--- src/userinterface/GUIController.java
+++ src/userinterface/GUIController.java
@@ -14,6 +14,7 @@ import javafx.scene.paint.Color;
 import javafx.stage.StageStyle;
 import model.BattleBoard;
 
+import java.io.FileNotFoundException;
 import java.util.Optional;
 
 import customThreads.GUIUpdateControlThread;
@@ -28,6 +29,9 @@ public class GUIController {
 
 	@FXML
 	private ScrollPane scrollPane3;
+
+	@FXML
+	private TextField howManyTxt;
 
 	@FXML
 	private TextField txtFRows1;
@@ -102,47 +106,44 @@ public class GUIController {
 
 			int rows2 = Integer.parseInt(txtFRows2.getText());
 			int columns2 = Integer.parseInt(txtFColumns2.getText());
-			
+
 			boolean repeat = false;
-			
-			
+
 			Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-	        alert.initStyle(StageStyle.UTILITY);
-	        alert.setTitle("Confirmation Dialog");
+			alert.initStyle(StageStyle.UTILITY);
+			alert.setTitle("Confirmation Dialog");
 			alert.setHeaderText("Please answer");
 			alert.setContentText("Can the numbers be repeated?");
 
 			ButtonType buttonTypeOne = new ButtonType("YES");
 			ButtonType buttonTypeTwo = new ButtonType("NO");
 			alert.getButtonTypes().setAll(buttonTypeOne, buttonTypeTwo);
-			
+
 			Optional<ButtonType> result = alert.showAndWait();
-			if(result.get() == buttonTypeOne) {
+			if (result.get() == buttonTypeOne) {
 				repeat = true;
 			}
-			
-			
 
 			battleBoard.generateRandomMatrix(rows1, columns1, rows2, columns2, repeat);
 			fillAndShowMatrix();
 
 		} catch (NumberFormatException e) {
 			Alert alert = new Alert(Alert.AlertType.WARNING);
-	        alert.initStyle(StageStyle.UTILITY);
-	        alert.setTitle("Information");
-	        alert.setHeaderText("WARNING!");
-	        alert.setContentText("Please proveide all of the information required to generate the matrix");
+			alert.initStyle(StageStyle.UTILITY);
+			alert.setTitle("Information");
+			alert.setHeaderText("WARNING!");
+			alert.setContentText("Please proveide all of the information required to generate the matrix");
 
-	       alert.showAndWait();
+			alert.showAndWait();
 		}
 
 	}
 
 	public void fillAndShowMatrix() {
-		
+
 		int[][] matrix1 = battleBoard.getMatrix1();
 		int[][] matrix2 = battleBoard.getMatrix2();
-		
+
 		GridPane gridX = new GridPane();
 		GridPane gridY = new GridPane();
 		gridX.setGridLinesVisible(true);
@@ -173,8 +174,8 @@ public class GUIController {
 
 				Label lx = new Label();
 
-				lx.setText(" "+matrix2[i][j]+" ");
-				
+				lx.setText(" " + matrix2[i][j] + " ");
+
 				grid2.add(lx, j, i);
 
 			}
@@ -182,65 +183,73 @@ public class GUIController {
 
 		scrollPane1.setContent(grid1);
 		scrollPane2.setContent(grid2);
-	
+
 	}
-	
 
     @FXML
-    public void multiply(ActionEvent event) {
+    void generateListOfMatrices1(ActionEvent event) {
+    	int n = Integer.parseInt(howManyTxt.getText());
+    	
     	try {
-        	battleBoard.componetToComponetMultiplier();
-        	int[][] result = battleBoard.getResult();
-        	
-        	GridPane gridX = new GridPane();
-        	gridX.setGridLinesVisible(true);
-        	grid3 = gridX;
-        	
-        	for (int i = 0; i < result.length; i++) {
-    			for (int j = 0; j < result[0].length; j++) {
-
-    				grid3.addColumn(i);
-    				grid3.addRow(j);
-    				
-    				Label lx = new Label();
-
-    				lx.setText(" " + result[i][j] + " ");
-    			
-    				if(battleBoard.isPrimeNumber(result[i][j]))
-    					lx.setTextFill(Color.DODGERBLUE);
-    				grid3.add(lx, j, i);
-
-    			}
-    		}
-        	
-        	scrollPane3.setContent(grid3);
-    	} 
-    	catch(NumberFormatException n) {
-    		Alert alert = new Alert(Alert.AlertType.WARNING);
-	        alert.initStyle(StageStyle.UTILITY);
-	        alert.setTitle("Information");
-	        alert.setHeaderText("WARNING!");
-	        alert.setContentText("Please proveide all of the information required");
-
-	       alert.showAndWait();
-    	}
-    	catch(NullPointerException e) {
-    		Alert alert = new Alert(Alert.AlertType.WARNING);
-	        alert.initStyle(StageStyle.UTILITY);
-	        alert.setTitle("Information");
-	        alert.setHeaderText("WARNING!");
-	        alert.setContentText("Please proveide all of the information required");
-
-	       alert.showAndWait();
-    	}
-    	
+			battleBoard.generateRamdonMatrices(n);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
-    
-    public void cleanResult() {
-    	
-    	GridPane gridX = new GridPane();
-    	scrollPane3.setContent(gridX);
-    	
-    }
+
+	@FXML
+	public void multiply(ActionEvent event) {
+		try {
+			battleBoard.componetToComponetMultiplier();
+			int[][] result = battleBoard.getResult();
+
+			GridPane gridX = new GridPane();
+			gridX.setGridLinesVisible(true);
+			grid3 = gridX;
+
+			for (int i = 0; i < result.length; i++) {
+				for (int j = 0; j < result[0].length; j++) {
+
+					grid3.addColumn(i);
+					grid3.addRow(j);
+
+					Label lx = new Label();
+
+					lx.setText(" " + result[i][j] + " ");
+					if (battleBoard.isPrimeNumber(result[i][j]))
+						lx.setTextFill(Color.DODGERBLUE);
+					grid3.add(lx, j, i);
+
+				}
+			}
+
+			scrollPane3.setContent(grid3);
+		} catch (NumberFormatException n) {
+			Alert alert = new Alert(Alert.AlertType.WARNING);
+			alert.initStyle(StageStyle.UTILITY);
+			alert.setTitle("Information");
+			alert.setHeaderText("WARNING!");
+			alert.setContentText("Please proveide all of the information required");
+
+			alert.showAndWait();
+		} catch (NullPointerException e) {
+			Alert alert = new Alert(Alert.AlertType.WARNING);
+			alert.initStyle(StageStyle.UTILITY);
+			alert.setTitle("Information");
+			alert.setHeaderText("WARNING!");
+			alert.setContentText("Please proveide all of the information required");
+
+			alert.showAndWait();
+		}
+
+	}
+
+	public void cleanResult() {
+
+		GridPane gridX = new GridPane();
+		scrollPane3.setContent(gridX);
+
+	}
 
 }
